7974734eb13e062891834a10734363bbe95a8a9c
pachara12 <69125290+pachara12@users.noreply.github.com>
2022-01-25 20:54:40 +0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,94 @@
+<?php 
+
+    session_start();
+    require_once 'config/db.php';
+    if (!isset($_SESSION['admin_login'])) {
+        $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ!';
+        header('location: signin.php');
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <div class="container">
+        <?php 
+
+            if (isset($_SESSION['admin_login'])) {
+                $admin_id = $_SESSION['admin_login'];
+                $stmt = $conn->query("SELECT * FROM users WHERE id = $admin_id");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        ?>
+        <h3 class="mt-3">Welcome Admin, <?php echo $row['firstname'] . ' ' . $row['lastname'] ?></h3>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+
+        
+    </div>
+    <body>
+    
+    <div class="container">
+    <h1 class="mt-5">Infomation Page</h1>
+    <hr>
+    <table id="mytable" class="table table-bordered table-striped">
+        <thead>
+            <th>#</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Phone number</th>
+            <th>Address</th>
+            <th>Posting Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </thead>
+
+        <tbody>
+            <?php 
+
+                include_once('functions.php');
+                $fetchdata = new DB_con();
+                $sql = $fetchdata->fetchdata();
+                while($row = mysqli_fetch_array($sql)) {
+
+            ?>
+
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['firstname']; ?></td>
+                    <td><?php echo $row['lastname']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['phonenumber']; ?></td>
+                    <td><?php echo $row['address']; ?></td>
+                    <td><?php echo $row['postingdata']; ?></td>
+                    <td><a href="update.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Edit</a></td>
+                    <td><a href="delete.php?del=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
+                </tr>
+
+            <?php 
+
+                }
+            ?>
+        </tbody>
+    </table>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ user.php
@@ -0,0 +1,94 @@
+<?php 
+
+    session_start();
+    require_once 'config/db.php';
+    if (!isset($_SESSION['user_login'])) {
+        $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ!';
+        header('location: signin.php');
+    }
+
+?>
+<?php 
+
+include_once('functions.php');
+
+$insertdata = new DB_con();
+
+if (isset($_POST['insert'])) {
+    $fname = $_POST['firstname'];
+    $lname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $phonenumber = $_POST['phonenumber'];
+    $address = $_POST['address'];
+    
+    $sql = $insertdata->insert($fname, $lname, $email, $phonenumber, $address);
+
+    if ($sql) {
+        echo "<script>alert('Record Inserted Successfully!');</script>";
+        echo "<script>window.location.href='user.php'</script>";
+    } else {
+        echo "<script>alert('Something went wrong! Please try again!');</script>";
+        echo "<script>window.location.href='insert.php'</script>";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <?php 
+
+            if (isset($_SESSION['user_login'])) {
+                $user_id = $_SESSION['user_login'];
+                $stmt = $conn->query("SELECT * FROM users WHERE id = $user_id");
+                $stmt->execute();
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            }
+        ?>
+        <h3 class="mt-4">Welcome, <?php echo $row['firstname'] . ' ' . $row['lastname'] ?></h3>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+    </div>
+    <div class="container">
+        
+        <hr>
+        <h1 class="mt-5">Insert Page</h1>
+        <hr>
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="firstname" class="form-label">First name</label>
+                <input type="text" class="form-control" name="firstname" required>
+            </div>
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Last name</label>
+                <input type="text" class="form-control" name="lastname" required>
+            </div>
+            <div class="mb-3">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="phonenumber">Phone Number</label>
+                <input type="text" class="form-control" name="phonenumber" required>
+            </div>
+            <div class="mb-3">
+                <label for="address">Address</label>
+                <textarea name="address"cols="30" rows="10" class="form-control" required></textarea>
+            </div>
+            <button type="submit" name="insert" class="btn btn-success">Insert</button>
+        </form>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+</body>
+</html>
